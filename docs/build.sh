66ee844bd5bd666f79f17e4d089a3f378191afb8
Avinash Rai <78463702+avinashhhhh@users.noreply.github.com>
2021-11-24 11:42:56 +0530

    Update build.sh

--- docs/build.sh
+++ docs/build.sh
@@ -11,6 +11,7 @@ source ../ci/rust-version.sh
 ../ci/docker-run.sh "$rust_stable_docker_image" docs/convert-ascii-to-svg.sh
 ./set-solana-release-tag.sh
 
+
 # Get current channel
 eval "$(../ci/channel-info.sh)"
 
